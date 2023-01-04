b199f8237aa73747314bba8ddad940d9802f1210
sebmagia <senunez@udec.cl>
2023-01-04 20:33:32 -0300

    muchas removidas del repo

--- big_matrix.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-nrows, ncols = 1000000, 100
-
-f = np.memmap('memmapped.dat', dtype=np.float32,
-              mode='w+', shape=(nrows, ncols))
--- ncf_fit.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-import functions as funcs
-
-data=np.loadtxt()
